6656a6a98c32f351ffdbc9184e2cc9e5bbb179c6
Thiagokairala <thiagor.kairala@gmail.com>
2014-04-26 13:48:14 -0300

    commiting.

---
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/internalization/Internalization_En.java
@@ -0,0 +1,29 @@
+package br.com.MDSGPP.ChamadaParlamentar.internalization;
+
+import java.util.ListResourceBundle;
+
+public class Internalization_En extends ListResourceBundle {
+
+	@Override
+	protected Object[][] getContents() {
+		return contents;
+	}
+	
+	static final Object[][] contents = {
+		  {"menu.Home", "Home"},
+		  
+		  {"menu.Deputys", "Deputys"},
+		  	{"menu.TrackDeputy", "Track Deputy"},
+		  
+		  {"menu.Party", "Party"},
+		  	{"menu.TrackParty", "Track Party"},
+		  
+		  {"menu.Sessions", "Sessions"},
+		  	{"menu.TrackSession", "Track Session"},
+		  
+		  {"menu.Ranking", "Ranking"},
+		  	{"menu.RankingTop5", "Top 5 Ranking"},
+		  	{"menu.CompleteRanking", "Complete Ranking"}
+	};
+
+}
