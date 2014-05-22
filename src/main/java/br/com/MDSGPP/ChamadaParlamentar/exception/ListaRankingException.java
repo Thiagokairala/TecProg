e4990592681ab09c453f82cb3200c800c16b1cff
RafaelFazzolino <rafael_fazzolino@hotmail.com>
2014-05-22 20:24:49 -0300

    Refatoring the class

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/ListaRankingException.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/ListaRankingException.java
@@ -1,13 +1,5 @@
 /**
  * Class: RankingListException.
- * 
- * @author Thiago Ramires Kairala
- * @author Thabata Helen Macedo Granja
- * @author Eduardo Brasil Martins
- * @author João Guilherme Santana Araruna
- * @author Nilton Cesar Campos Araruna
- * @author Rafael Fazzolino Pinto Barbosa
- * @author Bruno Contessotto Bragança Pinheiro
  * Date: march 26 2014.
  * 
  * Is class is the class that verify if the ranking had a problem to be
