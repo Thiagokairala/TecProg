ccc2e9c1c8ceba284a09261163fc3df0664e229d
Thabata Helen Macedo Granja <thabata.helen@gmail.com>
2014-05-23 20:59:26 -0300

    Change header modified:   src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/ListaVaziaException.java

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/ListaVaziaException.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/ListaVaziaException.java
@@ -1,16 +1,7 @@
 /**
  * Class: EmptyListException.
  * 
- * @author Thiago Ramires Kairala
- * @author Thabata Helen Macedo Granja
- * @author Eduardo Brasil Martins
- * @author João Guilherme Santana Araruna
- * @author Nilton Cesar Campos Araruna
- * @author Rafael Fazzolino Pinto Barbosa
- * @author Bruno Contessotto Bragança Pinheiro
- * Date: march 26 2014.
- * 
- * Is class is the class that verify if the deputy has valid data or
+ *Is class is the class that verify if the deputy has valid data or
  * not.
  */
 package br.com.MDSGPP.ChamadaParlamentar.exception;
