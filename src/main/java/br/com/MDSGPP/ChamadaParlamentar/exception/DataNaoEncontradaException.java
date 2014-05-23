849c4a9ecc5b392414efb002b3b46f075c456990
jgararuna <jgararuna@hotmail.com>
2014-05-23 11:02:48 -0300

    refatored header

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/DataNaoEncontradaException.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/exception/DataNaoEncontradaException.java
@@ -1,13 +1,6 @@
 /**
  * Class: DateNotFoundException
  * 
- * @author Thiago Ramires Kairala
- * @author Thabata Helen Macedo Granja
- * @author Eduardo Brasil Martins
- * @author João Guilherme Santana Araruna
- * @author Nilton Cesar Campos Araruna
- * @author Rafael Fazzolino Pinto Barbosa
- * @author Bruno Contessotto Bragança Pinheiro
  * Date: march 26 2014.
  * 
  * Exception that triggers the exception if the date is not found.
