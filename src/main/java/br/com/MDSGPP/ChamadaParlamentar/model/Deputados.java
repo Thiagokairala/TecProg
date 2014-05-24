ab2089ca938511243a546a2f0ff18ace54a0d75e
Thabata Helen Macedo Granja <thabata.helen@gmail.com>
2014-05-23 21:08:09 -0300

    Change header modified:   src/main/java/br/com/MDSGPP/ChamadaParlamentar/model/Deputados.java

--- src/main/java/br/com/MDSGPP/ChamadaParlamentar/model/Deputados.java
+++ src/main/java/br/com/MDSGPP/ChamadaParlamentar/model/Deputados.java
@@ -1,14 +1,5 @@
 /**
  * Class: UpdateDataBase
- * @author Thiago Ramires Kairala
- * @author Thabata Helen Macedo Granja
- * @author Eduardo Brasil Martins
- * @author João Guilherme Santana Araruna
- * @author Nilton Cesar Campos Araruna
- * @author Rafael Fazzolino Pinto Barbosa
- * @author Bruno Contessotto Bragança Pinheiro
- * Date: march 26 2014.
- * 
  * This class is the class that will hold all the attributes of Deputies.
  */
 package br.com.MDSGPP.ChamadaParlamentar.model;
